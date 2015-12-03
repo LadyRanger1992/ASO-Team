688c2483e6785c839ec6642c4b2ceba1985f6e12
LadyRanger1992 <luuvanluc@gmail.com>
2015-12-03 09:23:41 +0700

    Fist commit

---
+++ UIViewAnimation/UIViewAnimation/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  UIViewAnimation
+//
+//  Created by LucChelsea on 12/3/15.
+//  Copyright © 2015 LucChelsea. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ UIViewAnimation/UIViewAnimation/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  UIViewAnimation
+//
+//  Created by LucChelsea on 12/3/15.
+//  Copyright © 2015 LucChelsea. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
